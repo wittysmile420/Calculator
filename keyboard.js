628fbee32415fb04fbf1fa62d3c700c9999d01ac
wittysmile420 <219251886+wittysmile420@users.noreply.github.com>
2025-07-10 18:12:26 +0530

    Updated keyboard glow and sound glitch

--- keyboard.js
+++ keyboard.js
@@ -22,46 +22,66 @@
 // input_val[8] = document.querySelector(".eight");
 // input_val[9] = document.querySelector(".nine");
 
+function button_active_glow(button){
+    button.classList.add("button_active");
+
+    setTimeout(function(){
+        button.classList.remove("button_active");
+    }, 2*1000);
+}
+
 window.addEventListener("keyup",
     function(details){
         // key press = details.data
         let index_val = Number(details.key);
-        if(index_val >= 0 && index_val <= 9){
+        if(index_val >= 0 && index_val <= 9 && details.key != ' '){
             input_val[index_val].click();
+            button_active_glow(input_val[index_val]);
             return;
         }
         if(details.key == 'C'|| details.key == 'c'){
             clear_btn.click();
+            button_active_glow(clear_btn);
             return;
         }
         if(details.key == "Backspace" || details.key == "Delete"){
             del_btn.click();
+            button_active_glow(del_btn);
             return;
         }
         if(details.key == '/'){
             divide_btn.click();
+            button_active_glow(divide_btn);
             return;
         }
         if(details.key == '*'){
             multiply_btn.click();
+            button_active_glow(multiply_btn);
             return;
         }
         if(details.key == '-'){
             subtract_btn.click();
+            button_active_glow(subtract_btn);
             return;
         }
         if(details.key == '+'){
             add_btn.click();
+            button_active_glow(add_btn);
             return;
         }
         if(details.key == '.'){
             dot_btn.click();
+            button_active_glow(dot_btn);
             returrn;
         }
         if(details.key == '=' || details.key == "Enter"){
             ans_btn.click();
+            button_active_glow(ans_btn);
             return;
         }
-        alert("Enter A Valid Operator or Number On Keybard Or Use Calculator UI's Digital Buttons");
+
+        if(details.key != "Shift"){
+            alert("Enter A Valid Operator or Number On Keybard Or Use Calculator UI's Digital Buttons");
+        }
     }
 );
